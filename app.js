d7e5b1a3618bd9fc87f1d32ceab5ffe9e23d9b1e
Huy <lee@Huys-MacBook-Pro.local>
2022-03-18 13:43:45 +0700

    change code

--- app.js
+++ app.js
@@ -29,19 +29,42 @@
 // );
 
 //! HW4
-checkUser = (user, pw) => {
-   let username = prompt('Username');
-   if (username != user) {
-      console.log('Cancled');
-   } else {
-      let passw = prompt('Password');
-      if ((username === user && passw === '') || passw == null) {
-         console.log('Đã huỷ');
-      } else if (username === user && passw != pw) {
-         console.log('Sai mật khẩu');
-      } else if (username === user && passw === pw) {
-         console.log('Chào mừng');
-      }
+// checkUser = (user, pw) => {
+//    let username = prompt('Username');
+//    if (username != user) {
+//       console.log('Cancled');
+//    } else {
+//       let passw = prompt('Password');
+//       if ((username === user && passw === '') || passw == null) {
+//          console.log('Đã huỷ');
+//       } else if (username === user && passw != pw) {
+//          console.log('Sai mật khẩu');
+//       } else if (username === user && passw === pw) {
+//          console.log('Chào mừng');
+//       }
+//    }
+// };
+// checkUser('Admin', 'mindX');
+
+//! HW5
+let myArr = [9, 100, 2, 50, 4, 100, 22, 34];
+// a/ So chia het cho 2
+myArr.forEach(function (element) {
+   if (element % 2 == 0) {
+      console.log(element);
    }
-};
-checkUser('Admin', 'mindX');
+});
+// b/ Tim cac so lon hon 7
+let value = myArr.filter(function (value) {
+   return value > 7;
+});
+console.log(value);
+// c/ Tim cac so nho hon 5
+let value2 = myArr.filter(function (x) {
+   return x < 5;
+});
+console.log(value2);
+// d/ Tim cac so nho hon 50 va nhan doi so dol len
+myArr.map(function (number) {
+   if (number < 50) console.log(number * 2);
+});
